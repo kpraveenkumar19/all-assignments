84c26e97c10044f63e210fa4e698772578dca30f
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2024-01-26 19:35:13 +0530

    Update palindrome.js

--- week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
+++ week-1/Week-1-assignment-with-tests/01-js/medium/palindrome.js
@@ -7,6 +7,16 @@
 */
 
 function isPalindrome(str) {
+  var first=0;
+  var last=str.length-1;
+  str=str.toLowerCase();
+  while(first<last){
+    if(str[first]!=str[last]){
+      return false;
+    }
+  first++;
+  last--;
+  }
   return true;
 }
 
