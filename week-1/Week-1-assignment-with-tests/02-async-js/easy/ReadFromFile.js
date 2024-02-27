3b05c45568a79b2f38810dc305001b98fd0c2283
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2024-02-28 00:02:33 +0530

    Update ReadFromFile.js

--- week-1/Week-1-assignment-with-tests/02-async-js/easy/ReadFromFile.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/easy/ReadFromFile.js
@@ -1,1 +1,30 @@
+const fs = require('fs');
+const filePath = 'example.txt';
 
+function readFromFile(filePath,callback){
+   fs.readFile(filePath,'utf8',(err,data)=>{
+     if(err){
+       callback(err);
+       return;
+     }
+     callback(null,data);
+   })
+}
+
+function expOperation(){
+  let result=0;
+  for(let i=0;i<1000000000;i++){
+    result+=Math.random();
+  }
+  console.log("expensive operation completed");
+}
+
+
+readFromFile(filePath,(err,data)=>{
+  if(err){
+    console.error("Error reading a File",err);
+    return;
+  }
+  console.log("File content :" , data);
+  expOperation();
+});
