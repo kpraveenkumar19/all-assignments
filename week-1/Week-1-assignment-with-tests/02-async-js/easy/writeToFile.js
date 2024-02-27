a2ce230a9589206db87ad3e18efc0f66f22de218
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2024-02-28 00:26:14 +0530

    Update writeToFile.js

--- week-1/Week-1-assignment-with-tests/02-async-js/easy/writeToFile.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/easy/writeToFile.js
@@ -1,1 +1,12 @@
-
+const fs = require("fs");//import module fs
+const content = "praveen kumar is a bad boy";
+const filePath = "example.txt";
+//fs.writeFile(filePath,content,callback fucntion) has three parameters filePath,content to write to the file and callback function which will be executed 
+//after wrting operation is done and any peice of code written  below it will be executed after writing operation starts (not finished) when writing is finished callback func executes
+fs.writeFile(filePath, content, (err) => {
+  if (err) {
+    console.error("Errroe found while writing to File :", err);
+  }
+  console.log("writing to file is successful");
+});
+// any code below it will be ecexuted first
