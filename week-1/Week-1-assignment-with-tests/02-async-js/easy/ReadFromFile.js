4eafa7b6567793796db94e62fb4ba6fa1647520a
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2024-02-28 00:07:23 +0530

    Update ReadFromFile.js

--- week-1/Week-1-assignment-with-tests/02-async-js/easy/ReadFromFile.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/easy/ReadFromFile.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fs = require('fs');//import fs library
 const filePath = 'example.txt';
 
+// calls fs.readFile() which reads file and after reading file call back function executes with err parameter if it has error else with (null,data) parameter and executes call back function
 function readFromFile(filePath,callback){
    fs.readFile(filePath,'utf8',(err,data)=>{
      if(err){
@@ -10,7 +11,7 @@ function readFromFile(filePath,callback){
      callback(null,data);
    })
 }
-
+// expensive operation which executes after file content is printed
 function expOperation(){
   let result=0;
   for(let i=0;i<1000000000;i++){
@@ -19,7 +20,7 @@ function expOperation(){
   console.log("expensive operation completed");
 }
 
-
+//calling funtion for reading a file and passing callback function as a argument
 readFromFile(filePath,(err,data)=>{
   if(err){
     console.error("Error reading a File",err);
