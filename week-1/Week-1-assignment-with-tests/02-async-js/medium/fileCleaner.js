7a1f40712078aba2f0b799a06ce472eb9823e90f
kpraveenkumar19 <121229511+kpraveenkumar19@users.noreply.github.com>
2024-02-29 22:39:24 +0530

    Update fileCleaner.js

--- week-1/Week-1-assignment-with-tests/02-async-js/medium/fileCleaner.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/medium/fileCleaner.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+// removes extrapaces in the string
+function cleanDatafn(str){
+  return str.replace(/\s+/g,' ').trim();
+}
+// this funcn reads from file then cleans the data then rewrites to the file and then logs it
+function fileCleaner(filePath){
+  // reading from a file
+  fs.readFile(filePath,'utf8',(err,data)=>{
+    if(err){
+      console.error("Error reading a file :",err);
+      return;
+    }
+    // returns a clean string with no extra spaces
+    const cleanData =  cleanDatafn(data);
+    // rewrites clean string to the file
+    fs.writeFile(filePath,cleanData,'utf8',(err)=>{
+      if(err){
+       console.error("error in writing to a file:",err);
+        return;
+      }
+      console.log(cleanData);
+    });
+  });
+}
 
+
+const filePath =  'example.txt';
+fileCleaner(filePath);
